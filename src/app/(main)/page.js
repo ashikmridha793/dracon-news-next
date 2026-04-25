38979eab94268b7d4adccb8e613124409ff05e10
Ashik Mridha <ashikmridha43@gmail.com>
2026-04-25 17:01:32 +0600

    make a dragon news page

---
+++ src/app/(main)/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h2 className="text-3xl bg-yellow-800">Home page</h2>
+  );
+}
---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoutFound = () => {
+    return (
+        <div>
+            <h2>this page is not found</h2>
+        </div>
+    );
+};
+
+export default NoutFound;
