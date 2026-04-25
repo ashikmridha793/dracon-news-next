b4f247873f1a74a611d8a9d2150645963b5f888d
Ashik Mridha <ashikmridha43@gmail.com>
2026-04-25 17:24:47 +0600

    implement google font

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const NoutFound = () => {
     return (
-        <div>
-            <h2>this page is not found</h2>
+        <div className='text-center h-[80vh] flex justify-center item-center flex-col gap-5'>
+            <h1 className='font-bold text-8xl text-green-950 text-center'>404 </h1>
+            <h2 className='text-xl font-semibold'>This page is not found</h2>
+            <Link href='/'><button className='btn btn-ghost bg-purple-900 text-white'>Go Home</button></Link>
         </div>
     );
 };
