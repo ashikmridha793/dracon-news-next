1930ac1391e823f83b89223c802265a91cd8321a
Ashik Mridha <ashikmridha43@gmail.com>
2026-04-28 19:17:29 +0600

    added better auth

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,89 @@
+"use client"
+import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const RegisterPage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const handleRegisterFunc = (data) => {
+        const {email, name, photo, password} = data;
+        console.log(email, photo, name, password)
+    }
+
     return (
-        <div>
-            Register
+        <div className='container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100'>
+            <div className='p-4 rounded-xl bg-white'>
+                <h2 className="font-bold text-3xl text-center mb-6">
+                    Register your account
+                </h2>
+
+                <form className='space-y-4' onSubmit={handleSubmit(handleRegisterFunc)}>
+
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Name</legend>
+                        <input
+                            type="Name"
+                            className="input"
+                            placeholder="Type Your Name"
+                            {...register("name", {
+                                required: `"Name fild is required"`
+                            })}
+                        />
+                        {errors.name && (
+                            <p className='text-red-600'>{errors.name.message}</p>
+                        )}
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Photo URL</legend>
+                        <input
+                            type="text"
+                            className="input"
+                            placeholder="Type Your Photo URL"
+                            {...register("photo", {
+                                required: `"photo fild is required"`
+
+                            })}
+                        />
+                        {errors.photo && (
+                            <p className='text-red-600'>{errors.photo.message}</p>
+                        )}
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Email</legend>
+                        <input
+                            type="email"
+                            className="input"
+                            placeholder="Type Your Email"
+                            {...register("email", {
+                                required: `"Email fild is required"`
+
+                            })}
+                        />
+                        {errors.email && (
+                            <p className='text-red-600'>{errors.email.message}</p>
+                        )}
+                    </fieldset>
+
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Password</legend>
+                        <input
+                            type="password"
+                            className="input"
+                            placeholder="Type your password"
+                            {...register("password", {
+                                required: `"Password fild is required"`
+
+                            })}
+                        />
+                        {errors.password && (
+                            <p className='text-red-600'>{errors.password.message}</p>
+                        )}
+                    </fieldset>
+
+                    <button className="btn w-full bg-slate-800 text-white">Register</button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
         <div className='flex justify-between container mx-auto gap-2 my-2'>
             <div></div>
             <ul className='flex gap-5 justify-between items-center'>
-                <li className='text-gray-700'><Navlink href="/">Home</Navlink></li>
-                <li className='text-gray-700'><Navlink href="/about-us">About</Navlink></li>
-                <li className='text-gray-700'><Navlink href="/career" className="text-purple-800">Career</Navlink></li>
+                <li className='text-gray-700 font-bold'><Navlink href="/">Home</Navlink></li>
+                <li className='text-gray-700 font-bold'><Navlink href="/about-us">About</Navlink></li>
+                <li className='text-gray-700 font-bold'><Navlink href="/career" className="text-purple-800">Career</Navlink></li>
             </ul>
             <div className='flex items-center gap-2'>
                 <Image src={userAvatar} alt='User abater' width={50} height={50} />
